36d61c779a8e2e433fd636df6be70a370bd7e679
Marcel Cruz <marcelcruz@live.com>
2018-06-28 21:21:50 +0100

    sass added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'normalize.css'
-import './styles/styles.css'
+import './styles/styles.scss'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
