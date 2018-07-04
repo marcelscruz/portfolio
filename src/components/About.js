6238854f8539191373b9df3326e9f78ee004e2a3
Marcel Cruz <marcelcruz@live.com>
2018-07-04 22:33:15 +0100

    changing About text

--- src/components/About.js
+++ src/components/About.js
@@ -10,10 +10,10 @@ class About extends Component {
         <div className="about__profile">
           <Fade bottom>
             <h3>
-              I'm a developer. A web developer. From Brazil, now living in
-              Ireland. I speak Portuguese, English and JavaScript. Front end and
-              back end, I like them both. Team player. Guitar player. Pool
-              player.
+              I'm a developer. A web developer. Responsive and cross-browser
+              websites and web apps, that's what I do. I speak Portuguese,
+              English and JavaScript. Front end and back end, I like them both.
+              Team player. Guitar player. Pool player.
             </h3>
           </Fade>
         </div>
